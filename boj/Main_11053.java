063305d57e2f0d36ff8b3913e93c204ec257cd55
ok2qw66 <ok2qw66@gmail.com>
2021-05-23 23:58:36 +0900

    _

---
+++ boj/Main_11053.java
@@ -0,0 +1,13 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_11053 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+
+}
