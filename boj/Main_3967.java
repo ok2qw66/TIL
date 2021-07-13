331a47e0c76b69b2551e941a7b93c375746484ba
ok2qw66 <ok2qw66@gmail.com>
2021-07-13 22:48:42 +0900

    boj 3967

--- boj/Main_3967.java
+++ boj/Main_3967.java
@@ -5,7 +5,8 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main_3967 {
-	private static boolean[] isUsed = new boolean[12];
+	private static boolean[] isUsed = new boolean[13];
+	private static boolean[] isFixed = new boolean[13];
 	private static int[] arr = new int[12];
 	private static int MAX = 12;
 	private static char[][] star = new char[5][9];
@@ -15,29 +16,31 @@ public class Main_3967 {
 		for (int i = 0; i < 5; i++) {
 			star[i] = br.readLine().toCharArray();
 			for (int j = 0; j < 9; j++) {
-				if(star[i][j]!='.' && star[i][j]!='x') {
-					int tt = star[i][j]-'A';
-					isUsed[tt] = true;
-					arr[idx++] = tt+1;
+				if(star[i][j]!='.') {
+					if(star[i][j]!='x') {
+						int tt = star[i][j]-'A';
+						isUsed[++tt] = true;
+						isFixed[tt] = true;
+						arr[idx] = tt;
+					}
+					idx++;
 				}
 			}
 		}
-		
 		findAnswer(0);
 
 	}
 	private static boolean findAnswer(int i) {
-		System.out.println(i);
-		if(i==MAX-1) return check();
+		if(i==MAX) return check();
 		
-		if(arr[i]!=0) {
+		if(isFixed[arr[i]]) {
 			return findAnswer(i+1);
 		}
 		
-		for (int j = 0; j < 12; j++) {
+		for (int j = 1; j <= 12; j++) {
 			if(!isUsed[j]) {
 				isUsed[j] = true;
-				arr[i] = j+1;
+				arr[i] = j;
 				if(findAnswer(i+1))
 					return true;
 				isUsed[j] = false;
@@ -56,17 +59,14 @@ public class Main_3967 {
 		t[4] = arr[11] + arr[8] + arr[5] + arr[1];
 		t[5] = arr[11] + arr[9] + arr[6] + arr[4];
 		
-		System.out.println(Arrays.toString(arr));
-		System.out.println(Arrays.toString(t));
-		
 		for (int i = 1; i < 6; i++) {
 			if(t[0] != t[i])
 				return false;
 		}
-		
+			
 		StringBuilder sb = new StringBuilder();
 		int idx = 0;
-		for (int i = 0; i < t.length; i++) {
+		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 9; j++) {
 				if(star[i][j]!='.') {
 					star[i][j] =  (char) (arr[idx++]-1 + 'A');
